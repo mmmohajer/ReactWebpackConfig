32c6e95b5735c9472c8c5f6fab45f041c2247a22
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-02-22 11:34:46 -0500

    Webpack is working well

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import "basedesign-iswad/dist/style.js";
 
 import "./assets/styles/main.scss";
 import AppRoutes from "./AppRoutes";
